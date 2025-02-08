31f8f0e5582a27f6806e9aa4a87850ffe32c7447
AndreM6169 <162633606+AndreM6169@users.noreply.github.com>
2025-02-07 22:38:57 -0300

    Add files via upload
    Primeiro código de JAVA feito em sala.

---
+++ OlaJava/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Olá Java!");
+
+        int x = 10;
+        int y = 20;
+        int z = x + y;
+
+        System.out.println("O valor de z é:" + z);
+    }
+}
