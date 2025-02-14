6ca1555d485447f52471c6360862108e269296dd
André Mesquita <162633606+AndreLomb@users.noreply.github.com>
2025-02-14 02:53:16 -0300

    Add files via upload

---
+++ Aula03/src/Ex1.java
@@ -0,0 +1,12 @@
+public class Ex1 {
+    public static void main(String[] args){
+        int lanche1 = 10, lanche2 = 4, lanche3 = 2;
+
+        int total = lanche1+lanche2+lanche3;
+        int media = total/3;
+
+        System.out.println("O total arrecadado foi: " + total);
+        System.out.println("A média arrecadada foi: " + media);
+
+    }
+}
